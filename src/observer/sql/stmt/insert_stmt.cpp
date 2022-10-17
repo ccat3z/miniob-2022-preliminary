0ebc8e1d775cb4aec05a92f0ecbf4db246020b95
ccat3z <fzhang.chn@foxmail.com>
2022-10-17 21:16:49 +0800

    insert: cast value

--- src/observer/sql/stmt/insert_stmt.cpp
+++ src/observer/sql/stmt/insert_stmt.cpp
@@ -52,6 +52,7 @@ RC InsertStmt::create(Db *db, const Inserts &inserts, Stmt *&stmt)
   for (int i = 0; i < value_num; i++) {
     const FieldMeta *field_meta = table_meta.field(i + sys_field_num);
     const AttrType field_type = field_meta->type();
+    values[i].try_cast(field_type);
     const AttrType value_type = values[i].type;
     if (field_type != value_type) { // TODO try to convert the value type to field type
       LOG_WARN("field type mismatch. table=%s, field=%s, field type=%d, value_type=%d", 
