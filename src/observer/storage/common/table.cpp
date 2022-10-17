ccat3z/miniob-2021-preliminary

storage: implement drop table

src/observer/storage/common/table.cpp
@@ -15,6 +15,7 @@ See the Mulan PSL v2 for more details. */
 #include <limits.h>
 #include <string.h>
 #include <algorithm>
+#include <cstdio>
 
 #include "storage/common/table.h"
 #include "storage/common/table_meta.h"
@@ -153,6 +154,36 @@ RC Table::open(const char *meta_file, const char *base_dir) {
     indexes_.push_back(index);
   }
   return rc;
+}
+
+RC Table::drop() {
+  // TODO: Recovery io error
+
+  // Remove meta files
+  std::string meta_path = table_meta_file(base_dir_.c_str(), table_meta_.name());
+  if (::remove(meta_path.c_str()) != 0) {
+    LOG_ERROR("Failed to delete table meta file: %s", meta_path.c_str());
+    return RC::IOERR_DELETE;
+  }
+
+  // Close and remove indexes
+  for (int i = 0; i < table_meta_.index_num(); i++) {
+    std::string index_path = index_data_file(base_dir_.c_str(), table_meta_.name(), table_meta_.index(i)->name());
+
+    if (::remove(index_path.c_str()) != 0) {
+      LOG_ERROR("Failed to delete index meta file: %s", index_path.c_str());
+      return RC::IOERR_DELETE;
+    }
+  }
+
+  // Remove table data
+  std::string data_file = base_dir_ + "/" + table_meta_.name() + TABLE_DATA_SUFFIX;
+  if (::remove(data_file.c_str()) != 0) {
+      LOG_ERROR("Failed to delete data file: %s", data_file);
+      return RC::IOERR_DELETE;
+  }
+
+  return RC::SUCCESS;
 }
 
 RC Table::commit_insert(Trx *trx, const RID &rid) {

src/observer/storage/default/default_storage_stage.cpp
@@ -244,6 +244,13 @@ void DefaultStorageStage::handle_event(StageEvent *event) {
       snprintf(response, sizeof(response), "%s", result.c_str());
     }
     break;
+  case SCF_DROP_TABLE: {
+      const char *table_name = sql->sstr.drop_table.relation_name;
+      rc = handler_->drop_table(current_db, table_name);
+      snprintf(response, sizeof(response), "%s\n", rc == RC::SUCCESS ? "SUCCESS" : "FAILURE");
+      break;
+    }
+    break;
   default:
       snprintf(response, sizeof(response), "Unsupported sql: %d\n", sql->flag);
       break;

ccat3z/miniob-2022-preliminary

src/observer/storage/common/table.cpp
@@ -15,6 +15,7 @@ See the Mulan PSL v2 for more details. */
 #include <limits.h>
 #include <string.h>
 #include <algorithm>
+#include <cstdio>
 
 #include "common/defs.h"
 #include "storage/common/table.h"
@@ -183,6 +184,37 @@ RC Table::open(const char *meta_file, const char *base_dir, CLogManager *clog_ma
     clog_manager_ = clog_manager;
   }
   return rc;
+}
+
+RC Table::drop()
+{
+  // TODO: Recovery io error
+
+  // Remove meta files
+  std::string meta_path = table_meta_file(base_dir_.c_str(), table_meta_.name());
+  if (::remove(meta_path.c_str()) != 0) {
+    LOG_ERROR("Failed to delete table meta file: %s", meta_path.c_str());
+    return RC::IOERR_DELETE;
+  }
+
+  // Close and remove indexes
+  for (int i = 0; i < table_meta_.index_num(); i++) {
+    std::string index_path = table_index_file(base_dir_.c_str(), table_meta_.name(), table_meta_.index(i)->name());
+
+    if (::remove(index_path.c_str()) != 0) {
+      LOG_ERROR("Failed to delete index meta file: %s", index_path.c_str());
+      return RC::IOERR_DELETE;
+    }
+  }
+
+  // Remove table data
+  std::string data_file = base_dir_ + "/" + table_meta_.name() + TABLE_DATA_SUFFIX;
+  if (::remove(data_file.c_str()) != 0) {
+    LOG_ERROR("Failed to delete data file: %s", data_file);
+    return RC::IOERR_DELETE;
+  }
+
+  return RC::SUCCESS;
 }
 
 RC Table::commit_insert(Trx *trx, const RID &rid)

src/observer/storage/default/default_storage_stage.cpp
@@ -168,12 +168,19 @@ void DefaultStorageStage::handle_event(StageEvent *event)
       std::string result = load_data(dbname, table_name, file_name);
       snprintf(response, sizeof(response), "%s", result.c_str());
     } break;
+    case SCF_DROP_TABLE: {
+      const char *table_name = sql->sstr.drop_table.relation_name;
+      rc = handler_->drop_table(dbname, table_name);
+      snprintf(response, sizeof(response), "%s\n", rc == RC::SUCCESS ? "SUCCESS" : "FAILURE");
+    } break;
     default:
       snprintf(response, sizeof(response), "Unsupported sql: %d\n", sql->flag);
       break;
   }
 
-  if (rc == RC::SUCCESS && !session->is_trx_multi_operation_mode()) {
+  if (rc == RC::SUCCESS && !session->is_trx_multi_operation_mode() &&
+      sql->flag != SCF_DROP_TABLE  // TODO: Clear table ops in trx
+  ) {
     rc = current_trx->commit();
     if (rc != RC::SUCCESS) {
       LOG_ERROR("Failed to commit trx. rc=%d:%s", rc, strrc(rc));