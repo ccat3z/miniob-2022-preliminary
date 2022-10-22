776826b27753e780c5dfbff7116dff036f749a72
ccat3z <fzhang.chn@foxmail.com>
2022-10-22 12:12:08 +0800

    storage: table: update: recovery data and index if failed

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -720,10 +720,6 @@ RC Table::update_record(Trx *trx, const char *attribute_name, const Value *value
   }
 
   // Do update
-  auto updater = [&](Record &record) {
-    memcpy(record.data() + field->offset(), value->data, field->len());
-    return RC::SUCCESS;
-  };
   Record record;
   for (auto rid_it = matched_rids.begin(); rid_it != matched_rids.end(); rid_it++) {
     rc = record_handler_->get_record(&(*rid_it), &record);
@@ -742,6 +738,16 @@ RC Table::update_record(Trx *trx, const char *attribute_name, const Value *value
     }
 
     // Update record
+    auto old_value = std::unique_ptr<char, decltype(free) *>((char *)malloc(field->len()), free);
+    auto updater = [&](Record &record) {
+      memcpy(old_value.get(), record.data() + field->offset(), field->len());
+      memcpy(record.data() + field->offset(), value->data, field->len());
+      return RC::SUCCESS;
+    };
+    auto recovery = [&](Record &record) {
+      memcpy(record.data() + field->offset(), old_value.get(), field->len());
+      return RC::SUCCESS;
+    };
     rc = record_handler_->update_record_in_place(&record.rid(), updater);
     if (rc != RC::SUCCESS) {
       LOG_ERROR("Failed to update record (rid=%d.%d). rc=%d:%s",
@@ -754,7 +760,20 @@ RC Table::update_record(Trx *trx, const char *attribute_name, const Value *value
     if (rc != RC::SUCCESS) {
       LOG_ERROR("Failed to insert indexes of record for update (rid=%d.%d). rc=%d:%s",
                 record.rid().page_num, record.rid().slot_num, rc, strrc(rc));
-      return rc;
+
+      rc = record_handler_->update_record_in_place(&record.rid(), recovery);
+      if (rc != RC::SUCCESS) {
+        LOG_ERROR("Failed to recovery value");
+        return rc;
+      }
+
+      rc = insert_entry_of_indexes(record.data(), record.rid());
+      if (rc != RC::SUCCESS) {
+        LOG_ERROR("Failed to recovery index");
+        return rc;
+      }
+
+      return RC::INVALID_ARGUMENT;
     }
   }
 
