ccat3z/miniob-2021-preliminary

support operator is [not] null

src/observer/sql/parser/yacc_sql.y
@@ -89,6 +89,7 @@ ParserContext *get_context(yyscan_t scanner)
 		INNER
 		JOIN
         WHERE
+		IS
         AND
 		NOT
 		ORDER
@@ -497,6 +498,26 @@ condition:
 	condition_expr comp_op condition_expr {
 		$$ = (Condition *) malloc(sizeof(Condition));
 		condition_init($$, $2, &$1, &$3);
+	}
+	| condition_expr IS NULL_VALUE {
+		$$ = (Condition *) malloc(sizeof(Condition));
+		Value null;
+		value_init_null(&null);
+		ConditionExpr null_expr;
+		null_expr.type = COND_EXPR_VALUE;
+		null_expr.value.value = null;
+
+		condition_init($$, IS_NULL, &$1, &null_expr);
+	}
+	| condition_expr IS NOT NULL_VALUE {
+		$$ = (Condition *) malloc(sizeof(Condition));
+		Value null;
+		value_init_null(&null);
+		ConditionExpr null_expr;
+		null_expr.type = COND_EXPR_VALUE;
+		null_expr.value.value = null;
+
+		condition_init($$, IS_NOT_NULL, &$1, &null_expr);
 	}
 	;
 condition_expr:

ccat3z/miniob-2022-preliminary

src/observer/sql/parser/yacc_sql.y
@@ -87,6 +87,7 @@ ParserContext *get_context(yyscan_t scanner)
         VALUES
         FROM
         WHERE
+		IS
         AND
         SET
         ON
@@ -484,7 +485,27 @@ condition:
 	expr comOp expr {
 		condition_init(&$$, $2, &$1, &$3);
 	}
-    ;
+	| expr IS NULL_VALUE {
+		Value null;
+		value_init_null(&null);
+
+		UnionExpr null_expr;
+		null_expr.type = EXPR_VALUE;
+		null_expr.value.value = null;
+
+		condition_init(&$$, IS_NULL, &$1, &null_expr);
+	}
+	| expr IS NOT NULL_VALUE {
+		Value null;
+		value_init_null(&null);
+
+		UnionExpr null_expr;
+		null_expr.type = EXPR_VALUE;
+		null_expr.value.value = null;
+
+		condition_init(&$$, IS_NOT_NULL, &$1, &null_expr);
+	}
+	;
 
 comOp:
   	  EQ { $$ = EQUAL_TO; }